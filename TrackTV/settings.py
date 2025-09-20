53e421cebbcbb22c45531b49a6b0822deee19210
Anuj Parajuli <parajulianuj246@gmail.com>
2025-09-20 15:51:49 +0545

    Fix ALLOWED_HOSTS to properly parse Render host

--- TrackTV/settings.py
+++ TrackTV/settings.py
@@ -14,7 +14,7 @@ DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
 # ALLOWED_HOSTS
 # Use environment variable if set, else allow localhost for dev
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(' ')
+ALLOWED_HOSTS = [host.strip() for host in os.environ.get('ALLOWED_HOSTS', '').split() if host]
 
 # Application definition
 INSTALLED_APPS = [
